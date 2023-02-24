4cf1aa6de60e1725498c0f65abff202675c1a382
LuisFelipeMTCS <luisfelipetoledo.profissional@gmail.com>
2023-02-24 13:43:26 -0300

    inclusao hoteis

--- app.py
+++ app.py
@@ -2,4 +2,41 @@ from flask import Flask
 from flask_restful import Resource, Api
 
 
+app = Flask(__name__)
+api = Api(app)
 
+hoteis = [
+    {
+        'hoteis':'alpha',
+        'nome':'Alpha Hotel',
+        'Estrelas': 4.3,
+        'diaria': 420.434,
+        'cidade':'Santa Cataria'
+    },
+    {
+        'hoteis':'bass',
+        'nome':'bass Hotel',
+        'Estrelas': 4.3,
+        'diaria': 380.434,
+        'cidade':'Santa '
+
+    },
+    {
+        'hoteis':'tico',
+        'nome':'tico Hotel',
+        'Estrelas': 4.3,
+        'diaria': 520.434,
+        'cidade':'Cataria'
+
+    }
+]
+
+class Hoteis(Resource):
+    def get(self):
+        return {'hoteis':hoteis}
+    
+api.add_resource(Hoteis,'/hoteis')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+        
